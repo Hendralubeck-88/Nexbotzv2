8a79cbc4a707c1ae91a96fa58157185cf889ad9c
Hendralubeck-88 <118050450+Hendralubeck-88@users.noreply.github.com>
2023-01-17 03:40:11 +0700

    Update +api.js

--- plugins/+api.js
+++ plugins/+api.js
@@ -1,8 +1,6 @@
 /*
-# elaina - UserBot
-# Credit Sc Ori By ZeeoneOfc
-# Subscribe Yt RflBotz Ofc
-# This file is a part of < https://github.com/zeeoneofc/elaina/ >
+# 
+# This file is a part of < https://github.com/Hendralubeck-88/Nexbotzv2.git >
 */ 
 
 // WhatsApp api
@@ -89,7 +87,7 @@ module.exports = elaina = async (elaina, mek) => {
 		const wit = moment(Date.now()).tz('Asia/Jayapura').locale('id').format('HH:mm:ss z')
 		const type = Object.keys(mek.message)[0]        
 		const cmd = (type === 'conversation' && mek.message.conversation) ? mek.message.conversation : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : (type == 'extendedTextMessage') && mek.message.extendedTextMessage.text ? mek.message.extendedTextMessage.text : ''.slice(1).trim().split(/ +/).shift().toLowerCase()
-		const prefix = /^[ø=|~!#$%^&.?/\\^z+*@,;]/.test(cmd) ? cmd.match(/^[ø=|~!#$%^&.?/\\^z+*,;]/gi) : '-'          	
+		const prefix = /^[Ã¸=|~!#$%^&.?/\\^z+*@,;]/.test(cmd) ? cmd.match(/^[Ã¸=|~!#$%^&.?/\\^z+*,;]/gi) : '-'          	
         body = (type === 'conversation' && mek.message.conversation.startsWith(prefix)) ? mek.message.conversation : (type == 'imageMessage') && mek.message.imageMessage.caption.startsWith(prefix) ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption.startsWith(prefix) ? mek.message.videoMessage.caption : (type == 'extendedTextMessage') && mek.message.extendedTextMessage.text.startsWith(prefix) ? mek.message.extendedTextMessage.text : (type == "buttonsResponseMessage") && mek.message[type].selectedButtonId ? mek.message[type].selectedButtonId : ''
 		budy = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : ''
 		var pes = (type === 'conversation' && mek.message.conversation) ? mek.message.conversation : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : (type == 'extendedTextMessage') && mek.message.extendedTextMessage.text ? mek.message.extendedTextMessage.text : ''
